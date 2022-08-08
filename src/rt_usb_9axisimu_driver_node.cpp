1e89d0a70d9c6531734d48249c5bacf4658d5bec
tomson784 <42789340+tomson784@users.noreply.github.com>
2022-08-08 19:46:28 +0900

    Update rt_usb_9axisimu_driver_node.cpp

--- src/rt_usb_9axisimu_driver_node.cpp
+++ src/rt_usb_9axisimu_driver_node.cpp
@@ -53,7 +53,7 @@ int main(int argc, char** argv)
   ros::param::get("~angular_velocity_stddev", imu_stddev_angular_velocity);
   double imu_stddev_magnetic_field = imu_consts.DEFAULT_MAGNETIC_FIELD_STDDEV;
   ros::param::get("~magnetic_field_stddev", imu_stddev_magnetic_field);
-  double hz = 30.0;
+  double hz = 100;
   ros::param::get("~hz", hz);
 
   RtUsb9axisimuRosDriver driver(imu_port);
