51a092bed782b0fa697c901581c9772d687e6330
tomson784 <tomohito040759@gmail.com>
2023-07-26 00:13:25 +0900

    up

--- src/rt_usb_9axisimu_driver_node.cpp
+++ src/rt_usb_9axisimu_driver_node.cpp
@@ -53,14 +53,14 @@ int main(int argc, char** argv)
   ros::param::get("~angular_velocity_stddev", imu_stddev_angular_velocity);
   double imu_stddev_magnetic_field = imu_consts.DEFAULT_MAGNETIC_FIELD_STDDEV;
   ros::param::get("~magnetic_field_stddev", imu_stddev_magnetic_field);
-  double hz = 100;
+  double hz = 50.0;
   ros::param::get("~hz", hz);
 
   RtUsb9axisimuRosDriver driver(imu_port);
   driver.setImuFrameIdName(imu_frame_id);
   driver.setImuStdDev(imu_stddev_linear_acceleration, imu_stddev_angular_velocity, imu_stddev_magnetic_field);
 
-  ros::Rate rate(hz);
+  ros::Time previous_time = ros::Time::now();
 
   if (driver.startCommunication())
   {
@@ -90,13 +90,14 @@ int main(int argc, char** argv)
     {
       while (ros::ok())
       {
-        ros::spinOnce();
-        rate.sleep();
         if (driver.readSensorData())
         {
           if (driver.hasRefreshedImuData())
           {
-            driver.publishImuData();
+            if ((ros::Time::now() - previous_time).toSec() > (1.0/hz)){
+              driver.publishImuData();
+              previous_time = ros::Time::now();
+            }
           }
         }
         else
